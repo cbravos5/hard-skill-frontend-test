77687c9322b21b5195bba2fa4c1bdfa3af21d1a7
cbravos5 <cnbravos5@gmail.com>
2022-07-28 17:43:11 -0300

    chore: vite configs for path resolution and imgs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgrPlugin from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  assetsInclude: ["**/*.png", "**/*.gif", "**/*.svg"],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
+});
