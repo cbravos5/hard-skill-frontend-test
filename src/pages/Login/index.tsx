f92246b47492e1fe454f813617853709f96515df
cbravos5 <cnbravos5@gmail.com>
2022-07-28 20:14:43 -0300

    feat: add form with react-hook-form

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,15 +1,50 @@
 import LeadSoftLogo from "@/assets/leadsoft_logotipo.svg";
+import { AxiosInstance } from "@/configs/axiosConfig";
+import React from "react";
 import { LoginContainer } from "./style";
+import { useForm } from "react-hook-form";
+import { GlassButton } from "@/components/GlassButton";
+
+interface FormData {
+  username: string;
+  password: string;
+}
 
 export const Login: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const submitHandler = handleSubmit(async (data) => {
+    console.log(data);
+
+    //AxiosInstance.post("/Auth/Login", {});
+  });
+
   return (
     <LoginContainer>
       <img src={LeadSoftLogo} alt="Logotipo LeadSoft" />
       <p>Sistema de gerência de pessoas</p>
-      <form action="login">
-        <input type="text" placeholder="Usuário" required />
-        <input type="password" placeholder="Senha" required />
-        <button type="submit">Login</button>
+      <form action="login" onSubmit={submitHandler}>
+        <div className="input">
+          <input
+            type="text"
+            {...register("username", { required: true })}
+            placeholder="Usuário"
+          />
+          {errors.username && <span>Campo obrigatório</span>}
+        </div>
+        <div className="input">
+          <input
+            type="password"
+            {...register("password", { required: true })}
+            placeholder="Senha"
+          />
+          {errors.password && <span>Campo obrigatório</span>}
+        </div>
+        <GlassButton className="submit" type="submit" label="login" />
       </form>
     </LoginContainer>
   );
--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -19,14 +19,12 @@ export const LoginContainer = styled.main`
   }
 
   form {
-    display: flex;
-    flex-direction: column;
-    gap: 0.675rem;
-    align-items: center;
+    .input {
+      margin: 10px 0;
+    }
   }
 
   input {
-    width: 70%;
     font-size: 1.275rem;
     padding: 2px;
 
@@ -35,37 +33,19 @@ export const LoginContainer = styled.main`
     }
   }
 
-  button {
-    background-color: ${({ theme }) => theme.tertiary};
-    border-radius: 3px;
-    border: 1px solid ${({ theme }) => theme.primary};
-    box-shadow: rgba(255, 255, 255, 0.7) 0 1px 0 0 inset;
-    box-sizing: border-box;
-    color: #39739d;
-    cursor: pointer;
-    padding: 8px 0.8em;
-    text-align: center;
-    text-decoration: none;
-    user-select: none;
-    -webkit-user-select: none;
-    touch-action: manipulation;
-    vertical-align: baseline;
-    white-space: nowrap;
-
-    &:hover,
-    &:focus {
-      background-color: ${({ theme }) => theme.secondary};
-      color: #2c5777;
+  .input {
+    width: 70%;
+    * {
+      display: block;
     }
-
-    &:focus {
-      box-shadow: 0 0 0 4px rgba(0, 149, 255, 0.15);
+    span {
+      color: red;
+      font-size: 0.675rem;
     }
+  }
 
-    &:active {
-      background-color: #a0c7e4;
-      box-shadow: none;
-      color: #2c5777;
-    }
+  .submit {
+    display: block;
+    margin: auto;
   }
 `;
