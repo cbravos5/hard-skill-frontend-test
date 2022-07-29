e7fa15712e822e2107dcef2e568575f9d61fced2
cbravos5 <cnbravos5@gmail.com>
2022-07-28 21:43:24 -0300

    feat: add axios request for login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -5,11 +5,17 @@ import { LoginContainer } from "./style";
 import { useForm } from "react-hook-form";
 import { GlassButton } from "@/components/GlassButton";
 import { Input } from "@/components/Input";
+import { toast } from "react-toastify";
 
 interface FormData {
   username: string;
   password: string;
 }
+
+const isValidEmail = (email: string) =>
+  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
 
 export const Login: React.FC = () => {
   const {
@@ -18,9 +24,22 @@ export const Login: React.FC = () => {
     formState: { errors },
   } = useForm<FormData>();
 
-  const submitHandler = handleSubmit(async (data) => {
-    console.log(data);
+  const submitHandler = handleSubmit(async ({ username, password }) => {
+    try {
+      const response = await AxiosInstance.post("/Auth/Login", {
+        username,
+        password,
+      });
+
+      if (response.status === 200) console.log("LOGADO");
+    } catch (error: any) {
+      console.log(error.response);
+      if (error.response.data) toast.error("Usuário e/ou senha inválidos");
+    }
   });
+
+  const handleUserValidation = (username: string) =>
+    isValidEmail(username) || "Usuário deve ser um email válido";
 
   return (
     <LoginContainer>
@@ -31,15 +50,20 @@ export const Login: React.FC = () => {
           className="input"
           type="text"
           placeholder="Usuário"
-          {...register("username", { required: true })}
-          error={errors.username ? true : false}
+          {...register("username", {
+            required: "Campo obrigatório",
+            validate: handleUserValidation,
+          })}
+          error={errors.username?.message}
         />
         <Input
           className="input"
           type="password"
           placeholder="Senha"
-          {...register("password", { required: true })}
-          error={errors.password ? true : false}
+          {...register("password", {
+            required: { value: true, message: "Campo obrigatório" },
+          })}
+          error={errors.password?.message}
         />
         <GlassButton className="submit" type="submit" label="login" />
       </form>
