86f0155e876e6233c593f28f5f1768feab74a92b
cbravos5 <cnbravos5@gmail.com>
2022-07-28 21:42:41 -0300

    feat: input com erros genericos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import { Login } from "./pages/Login";
+import { ThemeProvider } from "styled-components";
+import GlobalCSS from "./global.css";
+import { defaultTheme } from "./theme";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-  return <Login />;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalCSS />
+      <Login />
+      <ToastContainer autoClose={2500} style={{ borderRadius: "10px" }} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,14 +2,14 @@ import { forwardRef, InputHTMLAttributes } from "react";
 import { InputStyle } from "./style";
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-  error: boolean;
+  error: string | undefined;
 }
 
 export const Input = forwardRef<HTMLInputElement, Props>(
   ({ error, className, ...rest }, ref) => (
     <InputStyle className={className}>
       <input className={error ? "error" : ""} {...rest} ref={ref} />
-      {error && <span>Campo obrigatório</span>}
+      {error && <span>{error}</span>}
     </InputStyle>
   )
 );
