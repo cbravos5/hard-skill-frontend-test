8bd55436bcc611d730a4c74a5373706c5d0aa1c7
cbravos5 <cnbravos5@gmail.com>
2022-07-29 11:25:02 -0300

    feat: add protected route

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,28 @@ import { ThemeProvider } from "styled-components";
 import GlobalCSS from "./global.css";
 import { defaultTheme } from "./theme";
 import { ToastContainer } from "react-toastify";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+interface ProtectedProps {
+  isAuthenticated: boolean;
+  children: JSX.Element;
+}
+
+const ProtectedRoute = ({ isAuthenticated, children }: ProtectedProps) =>
+  isAuthenticated ? children : <Navigate to="/login" replace />;
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalCSS />
-      <Login />
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path="/home">
+          <ProtectedRoute isAuthenticated={true}>
+            <h1>HOME</h1>
+          </ProtectedRoute>
+        </Route>
+      </Routes>
       <ToastContainer autoClose={2500} style={{ borderRadius: "10px" }} />
     </ThemeProvider>
   );
