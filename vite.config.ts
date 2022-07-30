6cf5ba29ba18aa9cabe4b8e18637f2da89eadab0
cbravos5 <cnbravos5@gmail.com>
2022-07-29 23:31:33 -0300

    config: add app port variable

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: Number(process.env.APP_PORT) || 3002,
+  },
   assetsInclude: ["**/*.png", "**/*.gif", "**/*.svg"],
   plugins: [
     react(),
